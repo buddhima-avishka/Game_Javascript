662a1684dc2d41b3cc7c32bca03a3616c556c3b7
BLB Avishka <buddhimaliyanageba@gmail.com>
2025-03-12 04:47:27 +0530

    Set time to appear the shape

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 var start = new Date().getTime();
 
+function shapeAppear(){
+  document.getElementById("d").style.display = "block";
+  var start = new Date().getTime();
+}
+
+function shapeAppearDelay(){
+  setTimeout(shapeAppear, Math.random() * 1500);
+}
+
 function disappear(){
-  document.getElementById("d").style.display="none";
+  document.getElementById("d").style.display = "none";
   var end = new Date().getTime();
   var time = (end - start) / 1000;
-  alert(time + " seconds");
+  document.getElementById("time").innerHTML = time + " seconds";
+  shapeAppearDelay();
 }
+
+shapeAppearDelay();
 
 document.getElementById("d").onclick = disappear;
