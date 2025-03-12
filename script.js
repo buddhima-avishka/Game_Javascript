420ee62e6908f5f2037196489d35f82646a49848
BLB Avishka <buddhimaliyanageba@gmail.com>
2025-03-12 16:35:11 +0530

    Change shape and change colours

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 var start = new Date().getTime();
 
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 function shapeAppear(){
+  var top = Math.random() * 550;
+  var left = Math.random() * 450;
+  var width = Math.random() * 150;
+
+  if(Math.random() > 0.5){
+    document.getElementById("d").style.borderRadius = "50%";
+  }else{
+    document.getElementById("d").style.borderRadius = "0%";
+  }
+
+  document.getElementById("d").style.top = top + "px";
+  document.getElementById("d").style.left = left + "px";
+
+  document.getElementById("d").style.width = width + "px";
+  document.getElementById("d").style.height = width + "px";
+
+  document.getElementById("d").style.backgroundColor = getRandomColor();
+
   document.getElementById("d").style.display = "block";
-  var start = new Date().getTime();
+  start = new Date().getTime();
 }
 
 function shapeAppearDelay(){
